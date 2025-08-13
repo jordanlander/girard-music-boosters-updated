6cc05ada5017c1553a3fe22cce60156fa41f86c6
jordanlander <49052005+jordanlander@users.noreply.github.com>
2025-08-13 01:31:49 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/girard-music-boosters-updated/',
+  base: '/girard-music-boosters-updated/', // << Pages subpath
   plugins: [react()],
 })
